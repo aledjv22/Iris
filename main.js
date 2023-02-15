25c0ca8eaa8e40f6ecbd2ac02a487262924a3115
Victor Alejandro Diaz Jauregui <victor.diaz.jauregui@mi.unc.edu.ar>
2023-02-15 14:09:04 -0300

    8

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+//Creo los elementos
+const mensaje = document.getElementById("mensaje");
 // Obtener la fecha actual
 var hoy = new Date();
 // Establecer la fecha del cumpleaños
-var fechaCumple = new Date("2023-02-15");
+var fechaCumple = new Date("2023-02-16");
 // Obtener la diferencia de tiempo entre las dos fechas
 var diffTiempo = fechaCumple.getTime() - hoy.getTime();
 // Convertir la diferencia de tiempo a días
@@ -9,9 +11,9 @@ var diffDias = Math.floor(diffTiempo / (1000 * 60 * 60 * 24));
 
 // Mostrar el mensaje de cumpleaños si es el día del cumpleaños
 if (diffDias == 0) {
-    document.getElementById("mensaje").innerHTML = "¡Feliz Cumpleaños!";
+    mensaje.innerHTML = "¡Feliz Cumpleaños!";
     document.title = "Feliz cumpleaños Iris!!!🎉";
     document.getElementById("btn-regalo").classList.toggle("inactive");
 } else {
-    document.getElementById("mensaje").innerHTML = "Faltan " + diffDias + " días para tu cumpleaños";
+    mensaje.innerHTML = "Faltan " + diffDias + " días para tu cumpleaños";
 }
