d723a523bb8a950ade2a07527373f07cc7943922
Victor Alejandro Diaz Jauregui <victor.diaz.jauregui@mi.unc.edu.ar>
2023-02-15 21:38:56 -0300

    14

--- main.js
+++ main.js
@@ -6,21 +6,22 @@ const boton2 = document.querySelector(".btn-salida");
 // Obtener la fecha actual
 var hoy = new Date();
 var today = (hoy.getDate().toString()) + ((hoy.getMonth() + 1).toString());
-var cumple = "162";
+var cumple = "152";
 
 //funcion de muestra de mensajes
 function mostrarMensaje(){
     // Mostrar el mensaje de cumpleaños si es el día del cumpleaños
     if (today == cumple) {
-        mensaje.innerHTML = "¡Feliz Cumpleaños!";
-        document.title = "Feliz cumpleaños Iris!!!🎉";
-        boton.style.display = 'none';
-        mensaje.classList.toggle("inactive");
+        titulo.classList.toggle("inactive");
+        boton.classList.toggle("inactive");
+        mensaje.innerHTML = "Feliz cumpleaños Iris";
+        mensaje.classList.toggle("holis");
+        boton2.classList.toggle("inactive");
     } else {
         mensaje.innerHTML = "Aun falta para tu cumpleaños, espera un toque plis. 🙌";
         titulo.classList.toggle("inactive");
         boton.classList.toggle("inactive");
-        mensaje.classList.toggle("holis");
+        mensaje.classList.toggle("chau");
         boton2.classList.toggle("inactive");
     }
 }
